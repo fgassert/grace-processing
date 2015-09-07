e4e24fdf5d7f930634773c55ccf32800d0505721
fgassert <cowbox314@gmail.com>
2015-09-07 15:19:29 -0400

    wgs84 tif

---
+++ README.md
@@ -0,0 +1,14 @@
+### GRACE data processing
+
+Land grids from http://grace.jpl.nasa.gov
+Average over three sources
+All measurements in cm water equivalent
+
+```
+grace.tif
+
+band 1: slope
+band 2: r2 of slope
+band 3: p-value of slope
+band 4: base error (unaccounted for in slope estimates)
+```
--- process.py
+++ process.py
@@ -3,12 +3,16 @@
 import numpy as np
 import netCDF4 as nc
 import scipy.stats as stats
+import rasterio as rio
+from rasterio import Affine as A
 
 NETCDFS=['jpl.nc','csr.nc','gfz.nc']
 SCALER='scaler.nc'
 SLOPE='slope.csv'
 R2='r2.csv'
 P='p.csv'
+ERR='err.csv'
+OUT='grace.tif'
 
 def main():
     # load and average netcdfs
@@ -20,12 +24,19 @@ def main():
             arr = np.asarray(ds.variables['lwe_thickness']) / len(NETCDFS)
         else:
             arr += np.asarray(ds.variables['lwe_thickness']) / len(NETCDFS)
+
     # multiply by scale factor
     ds = nc.Dataset(SCALER,'r')
     print ds.variables.keys()
     scaler = np.asarray(ds.variables['SCALE_FACTOR'])
     print scaler.shape
     arr = arr*scaler
+
+    # extract error grids
+    m_err = np.asarray(ds.variables['MEASUREMENT_ERROR'])
+    l_err = np.asarray(ds.variables['LEAKAGE_ERROR'])
+    t_err = np.sqrt(m_err*m_err + l_err*l_err)
+
     # compute slopes, coefficients
     print arr.shape
     slope_arr = np.zeros(arr.shape[1:])
@@ -45,6 +56,30 @@ def main():
     np.savetxt(SLOPE,slope_arr,delimiter=',')
     np.savetxt(R2,r2_arr,delimiter=',')
     np.savetxt(P,p_arr,delimiter=',')
+    np.savetxt(ERR,t_err,delimiter=',')
+
+    # rescale to WGS84 and dump to tif bands
+    rows = arr.shape[1]
+    cols = arr.shape[2]
+    d = 1
+    transform = A.translation(-cols*d/2,-rows*d/2) * A.scale(d,d)
+    slope_arr = np.flipud(np.roll(slope_arr.astype(rio.float64),180))
+    r2_arr = np.flipud(np.roll(r2_arr.astype(rio.float64),180))
+    p_arr = np.flipud(np.roll(p_arr.astype(rio.float64),180))
+    t_err = np.flipud(np.roll(t_err.astype(rio.float64),180))
+
+    with rio.open(OUT, 'w',
+                  'GTiff',
+                  width=cols,
+                  height=rows,
+                  dtype=rio.float64,
+                  crs={'init': 'EPSG:4326'},
+                  transform=transform,
+                  count=4) as out:
+        out.write_band(1, slope_arr)
+        out.write_band(2, r2_arr)
+        out.write_band(3, p_arr)
+        out.write_band(4, t_err)
 
 if __name__ == "__main__":
     main()
